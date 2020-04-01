80001fac2682b923a51d6a56918f04ccdc2a5f78
Abhishek Maira <abhishekmaira1999@gmail.com>
2020-04-01 21:57:19 +0530

    updated code

---
+++ lib/app/home/jobs/add_job_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddJobPage extends StatefulWidget {
+  @override
+  _AddJobPageState createState() => _AddJobPageState();
+}
+
+class _AddJobPageState extends State<AddJobPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
