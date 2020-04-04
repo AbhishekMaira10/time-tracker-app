697174734f5ba66aa5403ee96f7a1d6e0c6313ee
Abhishek Maira <abhishekmaira1999@gmail.com>
2020-04-04 21:13:26 +0530

    added bottom navigation

---
+++ lib/app/home/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
+import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TabItem _currentTab = TabItem.jobs;
+
+  Map<TabItem, WidgetBuilder> get widgetBuilders {
+    return {
+      TabItem.jobs: (_) => JobsPage(),
+      TabItem.entries: (_) => Container(),
+      TabItem.account: (_) => Container(),
+    };
+  }
+
+  void _select(TabItem tabItem) {
+    setState(() {
+      _currentTab = tabItem;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoHomeScaffold(
+      currentTab: _currentTab,
+      onSelectTab: _select,
+      widgetBuilders: widgetBuilders,
+    );
+  }
+}
